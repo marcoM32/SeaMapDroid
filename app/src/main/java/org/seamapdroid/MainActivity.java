e82449edeebdc131d5b3ebb9272cf09d0b7c7941
marcoM32 <magliano32@gmail.com>
2018-10-11 08:49:55 +0200

    Updated the manifest file, and removed the portant mode lock

--- app/src/main/java/org/seamapdroid/MainActivity.java
+++ app/src/main/java/org/seamapdroid/MainActivity.java
@@ -95,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         recording = Boolean.FALSE;

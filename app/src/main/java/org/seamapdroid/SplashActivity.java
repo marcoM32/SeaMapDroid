695c5d3fe90047f9414c0e1b5f735e6c80fb7355
marcoM32 <magliano32@gmail.com>
2019-12-05 21:55:01 +0100

    Add new splash activity

---
+++ app/src/main/java/org/seamapdroid/SplashActivity.java
@@ -0,0 +1,46 @@
+/*
+ * SeaMapDroid - An Android application to consult the libre online maps OpenSeaMap (map.openseamap.org)
+ * Copyright (C) 2017 Marco Magliano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.seamapdroid;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        configSplash.setBackgroundColor(R.color.colorPrimary);
+        configSplash.setAnimCircularRevealDuration(1000);
+        configSplash.setLogoSplash(R.mipmap.ic_launcher);
+        configSplash.setTitleSplash(getString(R.string.app_name));
+        configSplash.setTitleTextSize(30f);
+        configSplash.setAnimTitleDuration(1000);
+        configSplash.setAnimTitleTechnique(Techniques.SlideInDown);
+    }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
